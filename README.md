00b1994185dba53f6f0ef331988f88027d881179
Phil <qft.gtr@gmail.com>
2023-12-06 10:28:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meson-to-demo-vue
+ 
---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { onMounted } from 'vue'
+import { ParticleNetwork, WalletEntryPosition } from '@particle-network/auth'
+import { ParticleProvider } from '@particle-network/provider'
+
+import MesonTo from '@mesonfi/to'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+onMounted(() => {
+  const particle = new ParticleNetwork({
+    projectId: import.meta.env.VITE_PARTICLE_PROJECT_ID,
+    clientKey: import.meta.env.VITE_PARTICLE_CLIENT_KEY,
+    appId: import.meta.env.VITE_PARTICLE_APP_ID,
+    chainName: 'Ethereum',
+    chainId: 1,
+    wallet: {
+      displayWalletEntry: true,
+      defaultWalletEntryPosition: WalletEntryPosition.BR,
+      supportChains: [{ id: 1, name: 'Ethereum' }, { id: 42161, name: 'Arbitrum' }],
+      customStyle: {},
+    }
+  })
+
+  const particleProvider = new ParticleProvider(particle.auth)
+
+  const meson2 = new MesonTo(window)
+  meson2.open({ id: 'demo2', provider: particleProvider }, 'iframe')
+})
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
